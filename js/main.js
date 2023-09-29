178f7027a3d954b687558582a6065941da9a515b
satheshimman <satheshimman17@gmail.com>
2023-09-29 19:32:27 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var menuu=document.getElementById("imman")
+ 
+   var menulist=document.getElementById("list");
+     menuu.addEventListener("click",()=>{
+		 
+		 
+		 menulist.classList.toggle("showmenu")
+		 
+		 
+	 })
+	 
+	 
+var typed = new Typed(".text",{
+	
+	strings:["Frontend Devoloper...","Web Devoloper..."],
+	typeSpeed:100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true
+	
+});	 
+	 
